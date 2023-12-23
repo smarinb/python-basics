d8bd6b8b2eae15fe61e75ed65e923b1d7869c789
Sergio Marín <sergemb87@gmail.com>
2023-12-23 11:10:50 +0100

    list update

---
+++ 04_lists.py
@@ -0,0 +1,16 @@
+### Lists ###
+
+my_list = list()
+my_other_list = []
+
+my_list = [24,54,3,80,91]
+
+my_other_list = [3, 3.14, "Sergio"]
+
+
+print(len(my_list))
+
+print(my_other_list)
+
+
+print(type(my_other_list),type(my_other_list))
